9f3a27bce66004bb8f6f6bcf7fda8bd3f5e01ddf
Ron Gross <ron.gross@gmail.com>
2013-09-03 17:58:38 +0300

    Fix contact form

--- index.js
+++ index.js
@@ -149,14 +149,12 @@ function textBoxBlur(textBox, def)
 }
 
 function submitEmail() {
-	isValid = true;
-	textBox = $("#mce-EMAIL", "#mc-embedded-subscribe-form");
-
-	if ((textBox.prop("value") == "") || (textBox.prop("value") == "Your e-mail")) {
+    var email = $("#mce-EMAIL").prop("value");
+    if ((email == "") || (email == "Your e-mail")) {
 		alert("Please enter an e-mail");
 		return false;
 	}
-    if (!isValidEmailAddress(textBox.prop("value")))
+    if (!isValidEmailAddress(email))
     {
         alert("Please enter a valid e-mail");
         return false;
@@ -171,27 +169,24 @@ function submitEmail() {
 }
 
 function submitContact() {
-    isValid = true;
-    name = $("#yourName", "#contactForm");
-    email = $("#yourEmail", "#contactForm");
-    message = $("#yourMEssage", "#contactForm");
+    var name = $("#yourName").val();
+    var email = $("#yourEmail").val();
+    var message = $("#yourMEssage").val();
 
-    if ((name.prop("value") == "") || (name.prop("value") == "Name")) {
+    if ((name == "") || (name == "Name")) {
         alert("Please enter your name");
         return false;
     }
-
-    if ((email.prop("value") == "") || (email.prop("value") == "E-mail address")) {
+    if ((email == "") || (email == "E-mail address")) {
         alert("Please enter your email");
         return false;
     }
-
-    if ((message.prop("value") == "") || (message.prop("value") == "Your message")) {
+    if ((message == "") || (message == "Your message")) {
         alert("Please enter your message");
         return false;
     }
 
-    if (!isValidEmailAddress(email.prop("value"))) {
+    if (!isValidEmailAddress(email)) {
         alert("Please enter a valid e-mail");
         return false;
     }
@@ -202,6 +197,10 @@ function submitContact() {
     $("#saveForm").attr("disable", true);
     return false;
 }
+
+$(function(){
+    $("#saveForm").click(submitContact);
+});
 
 function isValidEmailAddress(emailAddress) {
 	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
